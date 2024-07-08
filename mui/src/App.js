5920fb4d66d03641d1b3f9cda63ff6e1c68f0a97
akashhsiv <akashhsiv@gmail.com>
2024-07-08 10:40:07 +0530

    frontend updated

--- mui/src/App.js
+++ mui/src/App.js
@@ -1,15 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button} from "@mui/material";
+import { Button,Typography} from "@mui/material";
+import { Add, Settings } from '@mui/icons-material';
 
 function App() {
   return (
     <div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button variant="text">
+        Text
+      </Button>
+
+      <Button startIcon={<Settings />} 
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Settings
+      </Button>
+
+      <Button startIcon={<Add />} 
+        variant="contained" 
+        color="success" 
+        size="small"
+      >
+        Add New Post
+      </Button>
+
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+    <Typography variant="h1" component="p">
+    h1. Heading
+  </Typography>
+  <Button varient="contained" sx={{
+    backgroundColor:"skyblue",
+    color:"888",
+    margin:5,
+   "&:hover":{
+    backgroundColor:"lightblue"
+   },
+   "&:disabled":{
+    backgroundColor:"grey",
+    color:"white"
+   }
+
+  }}>My Unique Button
+  </Button>
+  
     </div>
   );
 }
 
 export default App;
+
