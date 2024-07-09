f18c397d7b2dbb180980171986eaff45de8f8140
akashhsiv <akashhsiv@gmail.com>
2024-07-09 08:09:37 +0530

    sidebar , feed , rightbar added

--- mui/src/App.js
+++ mui/src/App.js
@@ -1,56 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button, Typography } from "@mui/material";
-import { Add, Settings } from '@mui/icons-material';
-import styled from '@emotion/styled';
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
+import { Box, Container, Stack } from "@mui/material";
 
 function App() {
-  const BlueButton = styled(Button)({
-    backgroundColor: "skyblue",
-    color: "888",
-    margin: 5,
-    "&:hover": {
-      backgroundColor: "lightblue"
-    },
-    "&:disabled": {
-      backgroundColor: "grey",
-      color: "white"
-    }
-  });
-
-  return (
-    <div>
-      <Button variant="text">
-        Text
-      </Button>
-
-      <Button startIcon={<Settings />}
-        variant="contained"
-        color="otherColor"
-        size="small"
-      >
-        Settings
-      </Button>
-
-      <Button startIcon={<Add />}
-        variant="contained"
-        color="success"
-        size="small"
-      >
-        Add New Post
-      </Button>
-
-      <Button variant="outlined" disabled>
-        Outlined
-      </Button>
-
-      <Typography variant="h1" component="p">
-        h1. Heading
-      </Typography>
-
-      <BlueButton>my button</BlueButton>
-      <BlueButton>just a button</BlueButton>
-    </div>
+  return(
+ <Box>
+  {/*Navbar*/}
+  <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
+  <Sidebar/>
+  <Feed/>
+  <Rightbar/>
+  </Stack>
+    </Box>
   );
 }
 
--- mui/src/index.js
+++ mui/src/index.js
@@ -8,10 +8,7 @@ import { ThemeProvider } from '@emotion/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-        <App />
-    </ThemeProvider>
-  
+        <App />  
   </React.StrictMode>
 );
 
